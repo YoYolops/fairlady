4f2aad603a3e7970ac41d994f7f44f3954380f01
YoYolops <yohan.rodrigues@ccc.ufcg.edu.br>
2026-04-23 15:09:40 -0300

    Implement glifo ecdsa and rsa key pair generations

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,7 +1,12 @@
 // Create a glifo implementation for at least two cryptographic algorithms (check DLT repositories)
 // Given a hard-coded file path, encrypt it and send to Kubo node via RPC
 // Given an CID, retrieve a file from network and decrypt it
+use glifo::encrypter::handle_credentials;
+use tokio;
 
-pub fn main() {
-    println!("HELLO FROM FAIRLADY DAEMON");
+#[tokio::main]
+async fn main() {
+    if let Err(e) = handle_credentials().await {
+        eprintln!("Error generating keys: {}", e);
+    }
 }
